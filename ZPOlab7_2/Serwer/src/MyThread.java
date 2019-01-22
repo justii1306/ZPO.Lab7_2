82727edc3b00c70b9dba0da6c76d05330f3c1f0a
justii1306 <35031460+justii1306@users.noreply.github.com>
2019-01-22 15:15:16 +0100

    Update MyThread.java

--- ZPOlab7_2/Serwer/src/MyThread.java
+++ ZPOlab7_2/Serwer/src/MyThread.java
@@ -14,19 +14,19 @@ public class MyThread extends NotifyingThread {
 
     private int checkPrimes(){
         int result = 0;
-        for (int i = this.start; i <= this.end; i++)
-            if (isPrime(i))
-                ++result;
+        for (int i = this.start; i <= this.end; i++) //Przeszukaj cały przedział
+            if (isPrime(i)) //Jeśli liczba jest liczbą pierwszą
+                ++result; //Zwiększ wynik
         return result;
     }
 
-    private static boolean isPrime(int number){
-        if (number == 1) return false;
-        if ((number == 2) || (number == 3)) return true;
-        if (number % 2 == 0) return false;
-        for (int i = 3; i <= sqrt(number); i+=2)
+    private static boolean isPrime(int number){ //Metoda sprawdzająca czy dana liczba jest liczbą pierwszą
+        if (number == 1) return false; //1 nie jest pierwsza
+        if ((number == 2) || (number == 3)) return true; //2 i 3 jest
+        if (number % 2 == 0) return false; //Liczby parzyste nie są pierwsze
+        for (int i = 3; i <= sqrt(number); i+=2) //Podziel sprawdzaną liczbę przez wszystkie liczby nieparyste większe od 3 i mniejsze od pierwiastka z sprawdzanej liczby 
             if (number % i == 0) return false;
-        return true;
+        return true; //To będzie mieło miejsce tylko jeśli wszystkie poprzednie if'y nie były prawdziwe, oznacza to, że liczba jest pierwsza
     }
 
     public int getPrimeNumbers() {
@@ -38,7 +38,7 @@ public class MyThread extends NotifyingThread {
     }
 
     @Override
-    public void doRun() {
-        setPrimeNumbers(checkPrimes());
+    public void doRun() { //Tu rozpoczynamy
+        setPrimeNumbers(checkPrimes()); //Ustaw zmienną PrimeNumber (liczba znalezionych liczb pierwszych) jako wynik metody checkPrimes()
     }
 }
