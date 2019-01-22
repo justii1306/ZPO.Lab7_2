71361640b3511339a005c827ac1d7172d8652b7e
justii1306 <35031460+justii1306@users.noreply.github.com>
2019-01-22 15:11:04 +0100

    Update NotifyingThread.java

--- ZPOlab7_2/Serwer/src/NotifyingThread.java
+++ ZPOlab7_2/Serwer/src/NotifyingThread.java
@@ -2,28 +2,28 @@ import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 public abstract class NotifyingThread extends Thread{
-    private final Set<ThreadCompleteListener> listeners = new CopyOnWriteArraySet<ThreadCompleteListener>();
+    private final Set<ThreadCompleteListener> listeners = new CopyOnWriteArraySet<ThreadCompleteListener>(); //Przygotowujemy nasłuchiwacz, który będzie przechowywał informacje, czy wątki zakończyły pracę
 
-    public final void addListener(final ThreadCompleteListener listener) {
-        listeners.add(listener);
+    public final void addListener(final ThreadCompleteListener listener) { //Metoda dodająca wątek do nasłuchiwania
+        listeners.add(listener); 
     }
 
-    public final void removeListener(final ThreadCompleteListener listener) {
-        listeners.remove(listener);
+    public final void removeListener(final ThreadCompleteListener listener) { //Metoda usuwająca z nasłuchiwania (nie używamy)
+        listeners.remove(listener); 
     }
 
-    private final void notifyListeners() {
+    private final void notifyListeners() { //Metoda informująca, że wątek skończył pracę
         for (ThreadCompleteListener listener : listeners) {
             listener.notifyOfThreadComplete(this);
         }
     }
 
     @Override
-    public final void run() {
+    public final void run() { 
         try {
-            doRun();
+            doRun(); //Wykonaj metodę doRun() z klasy MyThread
         } finally {
-            notifyListeners();
+            notifyListeners(); //Po wykonaniu pracy poinformuj, że skończyliśmy pracę
         }
     }
 
